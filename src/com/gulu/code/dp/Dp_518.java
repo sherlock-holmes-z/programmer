ec5fc3ebfd8fcd6ffcf97923ae3b50c3de36fd03
gulu <1105026730@qq.com>
2024-10-24 23:37:36 +0800

    零钱兑换2

--- src/com/gulu/code/dp/Dp_518.java
+++ src/com/gulu/code/dp/Dp_518.java
@@ -15,9 +15,10 @@ public class Dp_518 {
 
     public static int change(int amount, int[] coins) {
         int[] dp = new int[amount + 1];
+        dp[0] = 1;
         for (int i = 0; i < coins.length; i++) {
-            for (int j = 1; j < dp.length && j >= coins[i]; j++) {
-                dp[j] = Math.max(dp[j - 1], dp[j - coins[i]] + 1);
+            for (int j = coins[i]; j < dp.length; j++) {
+                dp[j] += dp[j - coins[i]];
             }
         }
         return dp[amount];
