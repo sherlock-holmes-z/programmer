d3cea96183d10492a317d1b515ec416a184c4a4d
sherlock <1105026730@qq.com>
2024-11-06 16:19:55 +0800

    213  打家劫舍2

--- src/com/gulu/code/dp/Dp_213.java
+++ src/com/gulu/code/dp/Dp_213.java
@@ -17,18 +17,19 @@ public class Dp_213 {
         int[] dp1 = new int[nums.length];
         if (nums.length > 1) {
             dp0[0] = nums[0];
-            dp0[2] = Math.max(dp0[1], nums[1]);
+            dp0[1] = Math.max(nums[0], nums[1]);
+
             dp1[1] = nums[1];
         } else {
             return nums[0];
         }
         for (int i = 2; i < nums.length; i++) {
             if (i < nums.length - 1) {
-                dp0[i] = Math.max(dp1[i - 2] + nums[i], dp1[i - 1]);
+                dp0[i] = Math.max(dp0[i - 2] + nums[i], dp0[i - 1]);
             }
             dp1[i] = Math.max(dp1[i - 2] + nums[i], dp1[i - 1]);
         }
-        return Math.max(dp0[nums.length - 1], dp1[nums.length - 1]);
+        return Math.max(dp0[nums.length - 2], dp1[nums.length - 1]);
     }
 
     public int rob2(int[] nums) {
