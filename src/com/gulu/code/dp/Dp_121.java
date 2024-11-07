c3425261ee98b6260f4b6f897381c48f6ecd7383
sherlock <1105026730@qq.com>
2024-11-07 16:39:38 +0800

    121 买卖股票的最佳时机

---
+++ src/com/gulu/code/dp/Dp_121.java
@@ -0,0 +1,24 @@
+package com.gulu.code.dp;
+
+/**
+ * 买卖股票的最佳时机
+ *
+ * @author chocolate
+ * 2024/11/6 16:25
+ */
+public class Dp_121 {
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int min = prices[0];
+        int max = 0;
+        for (int i = 1; i < prices.length; i++) {
+            min = Math.min(min, prices[i]);
+            max = Math.max(max, prices[i] - min);
+        }
+        return max;
+    }
+}
